49a2ea98bb810bc4b477e50dd8e3f8b2c7897d0f
unknown <ranarr.dev@gmail.com>
2025-07-30 12:08:51 +0600

    port change

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = 3000;
+  const PORT = 8000;
   await app.listen(PORT, () => {
     console.log(`Server is running at port: ${PORT}`);
   });
