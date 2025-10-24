649dec50d0af7df35b57e93091ab8b37120c5d3c
unknown <ranarr.dev@gmail.com>
2025-10-24 22:37:32 +0600

    feat: origin added

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true, 
+    origin: ['http://localhost:3000', 'https://blog-front-end-xcsc.vercel.app'],
+    credentials: true,
   });
 
   app.useGlobalPipes(
